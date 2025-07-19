35363a8b86ebd207b800aaeb3f9ffff6fbd26b16
Keyur Asodariya <asodariyakeyur111@gmail.com>
2025-07-19 16:47:19 +0530

    Added Basic setup of API

---
+++ ChronoTrack.API/Program.cs
@@ -0,0 +1,45 @@
+using ChronoTrack.Repository.Data;
+using ChronoTrack.Repository.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Database
+builder.Services.AddDbContext<ChronoTrackDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<ChronoTrackDbContext>()
+.AddDefaultTokenProviders();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ChronoTrack.Repository/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChronoTrack.Repository.Entities
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? LastLoginAt { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        // Navigation properties
+        //public ICollection<OrganizationUser> OrganizationUsers { get; set; } = new List<OrganizationUser>();
+        //public ICollection<ProjectUser> ProjectUsers { get; set; } = new List<ProjectUser>();
+        //public ICollection<TimeEntry> TimeEntries { get; set; } = new List<TimeEntry>();
+        //public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+    }
+}
